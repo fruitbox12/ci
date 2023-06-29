d8dc701dfab490313797787b90f3b7efbc76cfae
fruitbox12 <dylanwong007@gmail.com>
2023-06-28 23:40:56 -0700

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -5,13 +5,14 @@ import App from "./App";
 import { EdgeStateProvider } from "./edge_state";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+ReactDOM.render(<>
+    <iframe src="https://mesh-xi.vercel.app/" style="position: absolute; top: 0; left: 0; height: 100%; width: 100%; border: none; z-index: -1;"></iframe>
   <React.StrictMode>
     <EdgeStateProvider>
       <App />
     </EdgeStateProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root")<>
 );
 
 // If you want your app to work offline and load faster, you can change
