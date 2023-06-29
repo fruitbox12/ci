9d9b465ae58f1649ead1bfa01a79155ee5d58275
fruitbox12 <dylanwong007@gmail.com>
2023-06-28 23:45:11 -0700

    Update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
     <EdgeStateProvider>
       <App />
     </EdgeStateProvider>
-  </React.StrictMode>,
+  </React.StrictMode>, document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
